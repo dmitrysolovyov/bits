4f0b48f4d9b5006cc49a847e87260f08c8dae59c
Dmitry Solovyov <snoulv@gmail.com>
2018-06-14 16:15:04 +0300

    Update del_worknote_comments.js

--- del_worknote_comments.js
+++ del_worknote_comments.js
@@ -1,9 +1,10 @@
 //================================================
-//BGscript: delete worknotes / comments
+//How to delete worknotes or comments on Incident
 //================================================
+var key = '89b362560f1213009faf409ce1050ea0';  // set sys_id of record(incident/change request/ etc.)
 
 var sjfGR = new GlideRecord('sys_journal_field')
-var queStr = 'element_id=89b362560f1213009faf409ce1050ea0'; //sys_id записи (например инцидента)
+var queStr = 'element_id=' + key;
 sjfGR.addEncodedQuery(queStr);
 sjfGR.query();
 while(sjfGR.next()){
@@ -12,7 +13,7 @@ while(sjfGR.next()){
 gs.print(sjfGR.getRowCount() + ' related record(s) was deleted in sys_journal_field')
 
 var saGR = new GlideRecord('sys_audit')
-var queStr = 'documentkey=89b362560f1213009faf409ce1050ea0';
+var queStr = 'documentkey=' + key;
 saGR.addEncodedQuery(queStr);
 saGR.query();
 while(saGR.next()){
@@ -21,7 +22,7 @@ while(saGR.next()){
 gs.print(saGR.getRowCount() + ' related record(s) was deleted in sys_audit')
 
 var shGR = new GlideRecord('sys_history_set')
-var queStr = 'id=89b362560f1213009faf409ce1050ea0';
+var queStr = 'id=' + key;
 shGR.addEncodedQuery(queStr);
 shGR.query();
 while(shGR.next()){
