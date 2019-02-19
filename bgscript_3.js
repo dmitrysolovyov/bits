5e08634997281d80d6f6600598594a7cb3d8dd0d
Dmitry Solovyov <snoulv@gmail.com>
2019-02-19 17:59:18 +0300

    Update bgscript_3.js

--- bgscript_3.js
+++ bgscript_3.js
@@ -1,27 +1,18 @@
-//background script  3.	для всех активных чендж тасков ченджа заасайненго на меня, номер которого заканчивается на 5, в шорт дескрипшн написать Скоро Лето!
-(function() {
-	
-	var gr = new GlideRecord('change_request'),
-		currentUser = '6816f79cc0a8016401c5a33be04be441';
-	
-	gr.addQuery('assigned_to', currentUser);//проверяем что заасайнен на меня
-	gr.addQuery('number', 'ENDSWITH', '5');//проверяем что номер заканчивается на 5
-	gr.query();
-	if (gr.next()){
-		gs.print('Total amount of change requests:' + gr.getRowCount());//для проверки выводим количество чейнджей
-		gs.print(gr.getValue('number'));//выводим номер чейнджа 
-		
-		var chngTask = GlideRecord('change_task');
-		chngTask.addQuery('active', true);//активные чендж таски
-		chngTask.query();
-
-		if (chngTask.next()) {
-			chngTask.short_description += 'Скоро лето! ';
-
-			chngTask.update();
-			gs.print(chngTask.getValue('number'));//выводиncz номер таска
-			gs.print(chngTask.getValue('short_description'));
-		};
-		
-	};
-})();
+//background script  3.	для всех активных чендж тасков ченджа заасайненго на меня, 
+//номер которого заканчивается на 5, в шорт дескрипшн написать Скоро Лето!
+var gr = new GlideRecord('change_request');
+gr.addQuery('assigned_to', gs.getUserID());
+gr.addQuery('number', 'ENDSWITH', '5');
+gr.query();
+while (gr.next()){
+    gs.print('total: '+ gr.getRowCount());
+    var ch = GlideRecord('change_task');
+    ch.addQuery('change_request', gr.sys_id);
+    ch.addQuery('active', true);
+    ch.query();
+    while (ch.next()) {
+        ch.short_description += 'Скоро лето! ';
+        ch.update();
+        gs.print('ch.short_description: '+ch.short_description);
+    };
+};
