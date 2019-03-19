a1ba6ee4c7b04f27c6b6c731f87d21530c590536
Dmitry Solovyov <snoulv@gmail.com>
2019-03-19 13:27:04 +0300

    Update setElementStyle.js

--- setElementStyle.js
+++ setElementStyle.js
@@ -9,3 +9,18 @@ function setElementStyle(objectID, propertyObject){
 //     'color':'greenyellow',
 //     'background':'rgba(0,0,0,0.75)'}
 // );
+
+
+//Servicenow g_form adapted looks similar:
+function setElementStyle(field_name, propertyObject) {
+    var c = g_form.getControl(field_name);
+    for (var property in propertyObject) {
+        c.style[property] = propertyObject[property];
+    }
+}
+// call example:
+// setElementStyle("description", {
+//     "background":"#2c2c39",
+//     "color":"greenyellow",
+//     "font-size":"larger"
+// });
